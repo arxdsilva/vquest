aa1b7593884052268d0406a269d7d8b1d9f71451
Arthur Silva <arxdsilva@gmail.com>
2018-12-15 00:36:59 -0200

    simple interface

--- client/main.go
+++ client/main.go
@@ -1,17 +1,24 @@
 package main
 
-import "github.com/gen2brain/raylib-go/raylib"
+import (
+	"github.com/arxdsilva/vquest/client/display"
+	"github.com/gen2brain/raylib-go/raylib"
+)
 
 func main() {
-	rl.InitWindow(800, 450, "vQuest")
+	d := display.New()
+	rl.InitWindow(int32(d.Width), int32(d.Height), "vQuest")
 	rl.SetTargetFPS(60)
+
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
-		rl.DrawText("Login:", 190, 200, 20, rl.LightGray)
-		rl.DrawText("Password:", 190, 180, 20, rl.LightGray)
+		tH1 := float32(d.Height) * 0.4
+		tH2 := float32(d.Height) * 0.5
+		wdt := float32(d.Width) * 0.3
+		rl.DrawText("Login:", int32(wdt), int32(tH1), 20, rl.LightGray)
+		rl.DrawText("Password:", int32(wdt), int32(tH2), 20, rl.LightGray)
 		rl.EndDrawing()
-		rl.ToggleFullscreen(true)
 	}
 	rl.CloseWindow()
 }
