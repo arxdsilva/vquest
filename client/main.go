94a7e87658f5aa50f69b83a1e59d8305c550cbca
Arthur Silva <arxdsilva@gmail.com>
2018-12-15 04:10:48 -0200

    client: rename login > user

--- client/main.go
+++ client/main.go
@@ -17,7 +17,7 @@ func main() {
 	rectHeight := float32(d.Height) * 0.05
 	tH1 := float32(d.Height) * 0.4
 	tH2 := float32(d.Height) * 0.5
-	loginRect := rl.Rectangle{
+	userRect := rl.Rectangle{
 		X:      rectX,
 		Y:      tH1,
 		Width:  rectWidth,
@@ -29,42 +29,42 @@ func main() {
 		Width:  rectWidth,
 		Height: rectHeight,
 	}
-	var loginMouse, passMouse bool
-	var loginLetters, passLetters []string
+	var userMouse, passMouse bool
+	var userLetters, passLetters []string
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
-		loginMouse = mouseCollision(loginRect)
+		userMouse = mouseCollision(userRect)
 		passMouse = mouseCollision(passRect)
-		if loginMouse || passMouse {
+		if userMouse || passMouse {
 			key := rl.GetKeyPressed()
 			if (key >= 32) && (key <= 125) {
-				if loginMouse && (len(loginLetters) < 10) {
-					loginLetters = append(loginLetters, fmt.Sprintf("%c", key))
+				if userMouse && (len(userLetters) < 10) {
+					userLetters = append(userLetters, fmt.Sprintf("%c", key))
 				}
 				if passMouse && (len(passLetters) < 10) {
 					passLetters = append(passLetters, fmt.Sprintf("%c", key))
 				}
 			}
 			if rl.IsKeyPressed(rl.KeyBackspace) {
-				if (loginMouse) && (len(loginLetters) > 0) {
-					loginLetters = loginLetters[:len(loginLetters)-1]
+				if (userMouse) && (len(userLetters) > 0) {
+					userLetters = userLetters[:len(userLetters)-1]
 				}
 				if (passMouse) && (len(passLetters) > 0) {
 					passLetters = passLetters[:len(passLetters)-1]
 				}
 			}
 		}
-		rl.DrawRectangleRec(loginRect, rl.LightGray)
+		rl.DrawRectangleRec(userRect, rl.LightGray)
 		rl.DrawRectangleRec(passRect, rl.LightGray)
-		drawOutlineOnCollision(loginRect)
+		drawOutlineOnCollision(userRect)
 		drawOutlineOnCollision(passRect)
-		lInt := loginRect.ToInt32()
-		rl.DrawText(strings.Join(loginLetters, ""), lInt.X+5, lInt.Y+(lInt.Height/2), 20, rl.Maroon)
+		lInt := userRect.ToInt32()
+		rl.DrawText(strings.Join(userLetters, ""), lInt.X+5, lInt.Y+(lInt.Height/2), 20, rl.Maroon)
 		pInt := passRect.ToInt32()
 		rl.DrawText(strings.Repeat("-", len(passLetters)), pInt.X+5, pInt.Y+(pInt.Height/2), 20, rl.Maroon)
 		rl.ClearBackground(rl.RayWhite)
 		wdt := float32(d.Width) * 0.3
-		rl.DrawText("Login:", int32(wdt), int32(tH1), 20, rl.LightGray)
+		rl.DrawText("User:", int32(wdt), int32(tH1), 20, rl.LightGray)
 		rl.DrawText("Password:", int32(wdt), int32(tH2), 20, rl.LightGray)
 		rl.EndDrawing()
 	}
