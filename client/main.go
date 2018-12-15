ca2d22123eed7ac6a5c2c39a8389239d1db2ae48
Arthur Silva <arxdsilva@gmail.com>
2018-12-15 01:14:15 -0200

    draw rectangles on structs

--- client/main.go
+++ client/main.go
@@ -10,17 +10,29 @@ func main() {
 	rl.InitWindow(int32(d.Width), int32(d.Height), "vQuest")
 	rl.SetTargetFPS(60)
 	recColor := rl.Color{R: 255, G: 50, B: 0, A: 172}
+	rectX := float32(d.Width) / 2
+	rectWidth := float32(d.Width) * 0.1
+	rectHeight := float32(d.Height) * 0.05
+	tH1 := float32(d.Height) * 0.4
+	tH2 := float32(d.Height) * 0.5
+	loginRect := rl.Rectangle{
+		X:      rectX,
+		Y:      tH1,
+		Width:  rectWidth,
+		Height: rectHeight,
+	}
+	passRect := rl.Rectangle{
+		X:      rectX,
+		Y:      tH2,
+		Width:  rectWidth,
+		Height: rectHeight,
+	}
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
-		tH1 := float32(d.Height) * 0.4
-		tH2 := float32(d.Height) * 0.5
 		wdt := float32(d.Width) * 0.3
-		rectX := float32(d.Width) / 2
-		rectWidth := float32(d.Width) * 0.1
-		rectHeight := float32(d.Height) * 0.05
-		rl.DrawRectangle(int32(rectX), int32(tH1), int32(rectWidth), int32(rectHeight), recColor)
-		rl.DrawRectangle(int32(rectX), int32(tH2), int32(rectWidth), int32(rectHeight), recColor)
+		rl.DrawRectangleRec(loginRect, recColor)
+		rl.DrawRectangleRec(passRect, recColor)
 		rl.DrawText("Login:", int32(wdt), int32(tH1), 20, rl.LightGray)
 		rl.DrawText("Password:", int32(wdt), int32(tH2), 20, rl.LightGray)
 		rl.EndDrawing()
