40581cc1fd6fd8328c44858944109919d5edcf0e
Arthur Silva <arxdsilva@gmail.com>
2018-12-18 18:11:31 -0200

    c/msg: add Active property to Client and fix communicate()

--- client/msg/message.go
+++ client/msg/message.go
@@ -3,25 +3,28 @@ package msg
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"net"
+	"strings"
 )
 
 type Client struct {
 	Conn     net.Conn
 	Response string
+	Active   bool
 }
 
-func (s *Client) Conn() (err error) {
-	s.Conn, err = net.Dial("tcp4", "127.0.0.1:3333")
+func (s *Client) Connect() (err error) {
+	s.Conn, err = net.Dial("tcp", "127.0.0.1:3333")
+	s.Active = true
 	return
 }
 
 func (s *Client) Communicate(msg string) (err error) {
-	fmt.Fprintf(s.Conn, msg)
-	resp, err := bufio.NewReader(conn).ReadString('\n')
-	if (err != nil) && (err != io.EOF) {
+	fmt.Fprintf(s.Conn, msg+"\n")
+	resp, err := bufio.NewReader(s.Conn).ReadString('\n')
+	if err != nil {
 		return
 	}
-	s.Response = resp
+	s.Response = resp[:strings.IndexByte(resp, '\n')]
+	return
 }
