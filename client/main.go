cc9bca3b7d95c64007de922b76d777403b0e2c44
Arthur Silva <arxdsilva@gmail.com>
2018-12-18 18:13:00 -0200

    client: simple server comm

--- client/main.go
+++ client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/arxdsilva/vquest/client/display"
+	"github.com/arxdsilva/vquest/client/msg"
 	"github.com/gen2brain/raylib-go/raylib"
 )
 
@@ -10,16 +11,25 @@ var clientState int
 const loginScreen = 0
 const connected = 1
 
+var resp string
+
 func main() {
 	d := display.New()
 	rl.InitWindow(int32(d.Width), int32(d.Height), "vQuest")
 	rl.SetTargetFPS(60)
 	loadLoginObjects(d)
+	c := &msg.Client{}
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
+		rl.ClearBackground(rl.RayWhite)
 		if clientState == 0 {
 			drawLoginScreen(d)
 		} else {
+			if !c.Active {
+				c.Connect()
+			}
+			c.Communicate(userName)
+			resp = c.Response
 			drawConnectedScreen(d)
 		}
 		rl.EndDrawing()
